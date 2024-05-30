0772837f81e1a2ec7e6b0f8e56292cb003748313
Scaryune <scaryune@gmail.com>
2024-05-30 14:36:56 +0100

    Nier

---
+++ C#/Aula 11 - While and do...While loop/aula11.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace loop{
+    class WhileLoop{
+        public static void Main(string[] args)
+        {
+            int i=1;
+            while (i<=5)
+            {
+                Console.WriteLine("C# For Loop : Iteration {0}",i);
+                i++;
+            }
+        }
+    }
+}
